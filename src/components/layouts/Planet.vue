5583f49029f719cac509540a9daa709106c1b1eb
Benjamin Mark <mquadrant@gmail.com>
2019-09-28 03:28:21 +0100

    created planet section

---
+++ src/components/layouts/Planet.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="section planet-section">
+    <h2 class="section-title">Popular Planets</h2>
+    <div class="planet-cards">
+      <vue-glide>
+        <vue-glide-slide v-for="i in 6" :key="i">
+          <PlanetCard />
+        </vue-glide-slide>
+      </vue-glide>
+    </div>
+  </div>
+</template>
+
+<script>
+import PlanetCard from "../PlanetCard";
+export default {
+  name: "PlanetSection",
+  components: { PlanetCard }
+};
+</script>
+
+<style scoped>
+.planet-cards {
+  display: flex;
+  flex-wrap: wrap;
+  margin-bottom: 10px;
+}
+</style>
