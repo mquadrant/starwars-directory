b6a2c0506027218bcb28812d68e59c9f3119a7d0
Benjamin Mark <mquadrant@gmail.com>
2019-09-28 16:32:32 +0100

    added item count on pagination

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,23 +1,35 @@
 <template>
   <div v-if="totalPages() > 0" class="pagination-wrapper">
-    {{currentPage+1}} of {{totalPages()}}
-    <span
-      v-if="showPreviousLink()"
-      v-on:click="updatePage(currentPage - 1)"
-      class="pagination-btn"
-    >
-      <font-awesome-icon :icon="['fas','chevron-left']" class="page-arrow" />
-    </span>
-
-    <span v-if="showNextLink()" v-on:click="updatePage(currentPage + 1)" class="pagination-btn">
-      <font-awesome-icon :icon="['fas','chevron-right']" class="page-arrow" />
-    </span>
+    {{start}} - {{start+visibleStarship.length}} of {{starships.length}}&nbsp;
+    <div class="pagination-item">
+      <span
+        v-bind:class="{'link-end':!showPreviousLink()}"
+        v-on:click="updatePage(currentPage - 1)"
+        class="pagination-btn"
+      >
+        <font-awesome-icon :icon="['fas','chevron-left']" class="page-arrow" />
+      </span>
+      <span
+        v-bind:class="{'link-end':!showNextLink()}"
+        v-on:click="updatePage(currentPage + 1)"
+        class="pagination-btn right"
+      >
+        <font-awesome-icon :icon="['fas','chevron-right']" class="page-arrow" />
+      </span>
+    </div>
   </div>
 </template>
 <script>
 export default {
   name: "Pagination",
-  props: ["starships", "currentPage", "pageSize"],
+  props: [
+    "starships",
+    "currentPage",
+    "pageSize",
+    "visibleStarship",
+    "start",
+    "end"
+  ],
   methods: {
     updatePage(pageNumber) {
       this.$emit("page:update", pageNumber);
@@ -36,4 +48,36 @@ export default {
 </script>
 
 <style scoped>
+.pagination-wrapper {
+  font-size: 18px;
+  color: #757575;
+}
+.pagination-item {
+  display: inline-block;
+  border: 2px solid #d8d8d8;
+  border-radius: 10px;
+}
+
+span.pagination-btn {
+  display: inline-flex;
+  cursor: pointer;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+}
+.link-end {
+  pointer-events: none;
+  color: #d8d8d8;
+}
+.pagination-btn {
+  width: 70px;
+  height: 50px;
+  vertical-align: middle;
+}
+.page-arrow {
+  font-size: 15px;
+}
+.pagination-btn.right {
+  border-left: 2px solid #d8d8d8;
+}
 </style>
