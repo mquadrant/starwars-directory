c48a7e41da766305bea9a20c43c53135d93d2182
Benjamin Mark <mquadrant@gmail.com>
2019-09-28 10:16:04 +0100

    created character card component

---
+++ src/components/CharacterCard.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="card">
+    <div class="row">
+      <div class="row-item">
+        <div class="card-img-bottom"></div>
+      </div>
+      <div class="row-item">
+        <div class="card-block">
+          <h4 class="card-title">Luke Skywalker</h4>
+          <h6 class="card-subtitle">
+            <em>Son of Anakin</em>
+          </h6>
+          <div
+            class="card-text"
+          >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi, magni. Omnis libero, repudiandae dolor aliquid aut aspernatur recusandae necessitatibus iusto eum modi repellat earum sint cumque porro nesciunt</div>
+          <a href="#" class="card-readmore">Read More</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "CharacterCard"
+};
+</script>
+<style scoped>
+.card {
+  margin: auto;
+  height: 18rem;
+  margin-bottom: 30px;
+  background-color: #f5f5f5;
+}
+.card-img-bottom {
+  color: #fff;
+  height: 18rem;
+  background: url("./../assets/character-1.jpg") center no-repeat;
+  background-size: cover;
+  width: 300px;
+}
+.row {
+  display: flex;
+}
+.card-block {
+  width: 250px;
+  text-align: left;
+  padding: 20px 15px;
+}
+.card-title {
+  font-size: 15px;
+  font-weight: 600;
+  color: #3e464c;
+}
+.card-subtitle {
+  font-size: 11px;
+}
+.card-text {
+  display: inline-block;
+  margin-top: 30px;
+  font-size: 12px;
+  line-height: 1.5em;
+  color: #3e464c;
+  font-weight: 50;
+  width: 200px;
+
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 6;
+  -webkit-box-orient: vertical;
+}
+.card-readmore {
+  font-size: 12px;
+  font-weight: 600;
+  border-bottom: 1px solid #3e464c;
+}
+</style>
