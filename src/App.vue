a6480fd0b2705165fd9adcce417b199fba1eb816
Benjamin Mark <mquadrant@gmail.com>
2019-09-28 02:03:02 +0100

    added starshipsection to app

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,54 @@
 <template>
   <div id="app" class="w-full">
     <Banner />
+    <div class="container mx-auto px-4">
+      <StarshipSection />
+    </div>
   </div>
 </template>
 
 <script>
 import Banner from "./components/layouts/Banner";
+import StarshipSection from "./components/layouts/Starship";
 export default {
   name: "app",
-  components: { Banner }
+  components: { Banner, StarshipSection }
 };
 </script>
 
-<style src="./css/main.css">
+<style src="./css/main.css"/>
+<style>
 body,
 html {
   height: 100%;
   margin: 0;
   font-family: Arial, Helvetica, sans-serif;
+}
+.section {
+  margin-top: 50px;
+  margin-bottom: 20px;
+  text-align: center;
+}
+.section .section-title {
+  font-size: 24px;
+  color: #3e464c;
+  font-weight: 700;
+  text-align: center;
+  margin-bottom: 30px;
+}
+h2.section-title {
+  display: inline-block;
+  padding-bottom: 8px;
+  position: relative;
+  margin: 0 auto;
+}
+h2.section-title:before {
+  content: "";
+  position: absolute;
+  width: 50%;
+  height: 2px;
+  bottom: 0;
+  left: 25%;
+  border-bottom: 4px solid #3e464c;
 }
 </style>
