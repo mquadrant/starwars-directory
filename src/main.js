34feaf443651666497da6eb4f7de59eb8c2fd0d6
Benjamin Mark <mquadrant@gmail.com>
2019-09-28 02:50:54 +0100

    added the vue glider globally

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
-
+//FontAwesome Library
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faSearch, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+//VueGlide Library
+import VueGlide from "vue-glide-js";
+import "vue-glide-js/dist/vue-glide.css";
+
+Vue.use(VueGlide);
 
 library.add(faSearch, faArrowRight);
 Vue.component("font-awesome-icon", FontAwesomeIcon);
