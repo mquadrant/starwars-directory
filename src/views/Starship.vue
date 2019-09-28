adba355aeef9cb55a7ef16b50acd81854f7789b1
Benjamin Mark <mquadrant@gmail.com>
2019-09-28 11:45:15 +0100

    created starship page

---
+++ src/views/Starship.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Banner />
+    <div class="container mx-auto px-4">
+      <StarshipSection />
+    </div>
+  </div>
+</template>
+<script>
+import Banner from "../components/layouts/Banner";
+import StarshipSection from "../components/layouts/Starship";
+export default {
+  name: "Starship",
+  components: { Banner, StarshipSection }
+};
+</script>
+
+<style lang="stylus" scoped></style>
