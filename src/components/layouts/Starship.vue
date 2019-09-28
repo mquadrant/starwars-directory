9d1d8890f66b030d4b647f74bd4e0c07981da7d8
Benjamin Mark <mquadrant@gmail.com>
2019-09-28 02:04:30 +0100

    starship section view

---
+++ src/components/layouts/Starship.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="section starship-section">
+    <h2 class="section-title">Popular Starships</h2>
+    <div class="starship-cards">
+      <StarshipCard />
+      <StarshipCard />
+      <StarshipCard />
+      <StarshipCard />
+      <StarshipCard />
+      <StarshipCard />
+    </div>
+  </div>
+</template>
+
+<script>
+import StarshipCard from "./../StarshipCard";
+export default {
+  name: "StarshipSection",
+  components: { StarshipCard }
+};
+</script>
+
+<style scoped>
+.starship-cards {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
