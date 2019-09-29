6813f3abc530f938a00e91813c64be20c496e50b
Benjamin Mark <mquadrant@gmail.com>
2019-09-30 00:06:12 +0100

    made the filter form on characters page dynamic

--- src/views/Characters.vue
+++ src/views/Characters.vue
@@ -1,21 +1,23 @@
 <template>
   <div>
-    <Banner />
+    <Banner v-on:searchTerm="searchTerm" />
     <div class="container mx-auto px-4" style="text-align: center">
       <div class="section character-section">
         <h2 class="section-title">Popular Characters</h2>
         <div class="filter-block container px-10">
           <div class="filter-item">
             <label>FILTER</label>
-            <select name="filterlist" form="filterform">
+            <select v-model="gender" class="select" @change="filterlist" name="filterlist">
+              <option value="all">All</option>
               <option value="male">Male</option>
               <option value="female">Female</option>
+              <option value="robot">Robot</option>
             </select>
             <font-awesome-icon :icon="['fas','sort-down']" class="select-arrow" />
           </div>
           <div class="filter-item" style="margin-left:60px;">
             <label>VIEW</label>
-            <select name="viewlist" form="viewform">
+            <select name="viewlist">
               <option value="grid">Grid</option>
               <option value="list">List</option>
             </select>
@@ -23,16 +25,28 @@
           </div>
         </div>
         <div class="character-cards">
-          <CharacterCard />
-          <CharacterCard />
+          <CharacterCard
+            v-bind:character="character"
+            v-bind:key="character.name"
+            v-for="character in filterCharacters"
+          />
         </div>
       </div>
-      <Pagination v-bind:data="starships" v-bind:start="start" v-on:page:update="updatePage" />
+      <Pagination
+        v-bind:data="characters"
+        v-bind:start="start"
+        v-bind:end="end"
+        v-on:page:update="updatePage"
+        v-bind:count="count"
+        v-bind:next="next"
+        v-bind:previous="previous"
+      />
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 import Banner from "../components/layouts/Banner";
 import Pagination from "../components/Pagination";
 import CharacterCard from "../components/CharacterCard";
@@ -41,86 +55,100 @@ export default {
   components: { Banner, Pagination, CharacterCard },
   data() {
     return {
-      starships: [
-        {
-          id: 0,
-          title: "Ghost 0",
-          description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit."
-        },
-        {
-          id: 1,
-          title: "Ghost 1",
-          description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit."
-        },
-        {
-          id: 2,
-          title: "Ghost 2",
-          description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit."
-        },
-        {
-          id: 3,
-          title: "Ghost 3",
-          description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit."
-        },
-        {
-          id: 4,
-          title: "Ghost 4",
-          description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit."
-        },
-        {
-          id: 5,
-          title: "Ghost 5",
-          description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit."
-        },
-        {
-          id: 6,
-          title: "Ghost 6",
-          description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit."
-        },
-        {
-          id: 7,
-          title: "Ghost 7",
-          description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit."
-        },
-        {
-          id: 8,
-          title: "Ghost 8",
-          description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit."
-        },
-        {
-          id: 9,
-          title: "Ghost 9",
-          description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit."
-        },
-        {
-          id: 10,
-          title: "Ghost 10",
-          description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit."
-        },
-        {
-          id: 11,
-          title: "Ghost 11",
-          description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit."
-        }
-      ],
-      nextId: 12,
-      currentPage: 0,
-      pageSize: 9,
-      start: 0,
-      visibleStarship: []
+      filterCharacters: [],
+      characters: [],
+      count: 0,
+      end: 0,
+      start: 1,
+      next: false,
+      previous: false,
+      page: true,
+      gender: "all"
     };
+  },
+  methods: {
+    searchTerm(term) {
+      this.filterlist(term);
+    },
+    filterlist(term) {
+      if (!term) {
+        this.filterCharacters = this.characters.filter(character => {
+          if (this.gender === "all") return true;
+          else if (this.gender === "robot")
+            return character.gender.toLowerCase() === "n/a";
+          else return character.gender.toLowerCase() === this.gender;
+        });
+      } else {
+        let re = new RegExp(term, "gi");
+
+        this.filterCharacters = this.characters.filter(character => {
+          if (this.gender === "all")
+            return (
+              true &&
+              (re.test(character.name) ||
+                re.test(character.skin_color) ||
+                re.test(character.eye_color))
+            );
+          else if (this.gender === "robot")
+            return (
+              character.gender.toLowerCase() === "n/a" &&
+              (re.test(character.name) ||
+                re.test(character.skin_color) ||
+                re.test(character.eye_color))
+            );
+          else
+            return (
+              character.gender.toLowerCase() === this.gender &&
+              (re.test(character.name) ||
+                re.test(character.skin_color) ||
+                re.test(character.eye_color))
+            );
+        });
+      }
+    },
+    updatePage(control) {
+      if (control === "next" && this.next) {
+        axios
+          .get(this.next)
+          .then(res => {
+            this.start = this.end + 1;
+            this.end += res.data.results.length;
+            this.next = res.data.next;
+            this.previous = res.data.previous;
+            this.characters = [...res.data.results];
+            this.filterlist();
+          })
+          // eslint-disable-next-line
+          .catch(err => console.log(err));
+      } else if (control === "previous" && this.previous) {
+        axios
+          .get(this.previous)
+          .then(res => {
+            this.end = this.start - 1;
+            this.start = this.end - res.data.results.length + 1;
+            this.next = res.data.next;
+            this.previous = res.data.previous;
+            this.characters = [...res.data.results];
+            this.filterlist();
+          })
+          // eslint-disable-next-line
+          .catch(err => console.log(err));
+      }
+    }
+  },
+  created() {
+    axios
+      .get("https://swapi.co/api/people")
+      .then(res => {
+        this.count = res.data.count;
+        this.next = res.data.next;
+        this.previous = res.data.previous;
+        this.end = res.data.results.length;
+        this.characters = [...res.data.results];
+        this.filterCharacters = [...this.characters];
+      })
+      // eslint-disable-next-line
+      .catch(err => console.log(err));
   }
 };
 </script>
