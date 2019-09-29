ecd2f70af2c0cb03e3aaa2a5ce0b9eb0b5a0ab2c
Benjamin Mark <mquadrant@gmail.com>
2019-09-29 18:28:08 +0100

    added link to planets page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,10 @@
       <router-link to="starships">
         <button class="view-more">VIEW MORE</button>
       </router-link>
-      <PlanetSection />
+      <PlanetSection v-bind:planets="planets" v-bind:isPlanetFetch="isPlanetFetch" />
+      <router-link to="planets">
+        <button class="view-more">VIEW MORE</button>
+      </router-link>
       <CharacterSection />
       <router-link to="characters">
         <button class="view-more">VIEW MORE</button>
@@ -31,16 +34,29 @@ export default {
   },
   data() {
     return {
-      starships: []
+      starships: [],
+      planets: [],
+      isPlanetFetch: false
     };
   },
   methods: {},
   created() {
+    //Getting Popular Starship
     axios
       .get("https://swapi.co/api/starships")
       .then(res => {
         this.starships = res.data.results;
         this.starships.splice(6);
+      })
+      // eslint-disable-next-line
+      .catch(err => console.log(err));
+    //Getting Popular Planet
+    axios
+      .get("https://swapi.co/api/planets")
+      .then(res => {
+        this.planets = res.data.results;
+        this.planets.splice(5);
+        this.isPlanetFetch = true;
       })
       // eslint-disable-next-line
       .catch(err => console.log(err));
