2d5a778230cda4af74b93c72ce1858da79562e1f
Benjamin Mark <mquadrant@gmail.com>
2019-09-29 07:31:42 +0100

    created recent starship component

---
+++ src/components/layouts/RecentStarship.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="section planet-section">
+    <div class="StarshipCard">
+      <vue-glide :bullet="true" :rewind="false">
+        <vue-glide-slide v-for="i in 3" :key="i">
+          <StarshipCard />
+        </vue-glide-slide>
+      </vue-glide>
+    </div>
+  </div>
+</template>
+
+<script>
+import StarshipCard from "../StarshipCard";
+export default {
+  name: "RecentStarship",
+  components: { StarshipCard }
+};
+</script>
+
+<style scoped>
+.planet-cards {
+  display: flex;
+  flex-wrap: wrap;
+  margin-bottom: 10px;
+}
+</style>
