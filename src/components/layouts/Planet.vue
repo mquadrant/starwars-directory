cdf237d17ef4a7930a6ee3668888c334612c8a01
Benjamin Mark <mquadrant@gmail.com>
2019-09-30 10:59:11 +0100

    made two version of glide for responsiveness

--- src/components/layouts/Planet.vue
+++ src/components/layouts/Planet.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="section planet-section">
     <h2 class="section-title">Popular Planets</h2>
-    <div class="planet-cards" v-if="isPlanetFetch">
+    <div class="planet-cards image--lg" v-if="isPlanetFetch">
       <vue-glide :perView="3" :bullet="true" :bound="true" :rewind="false">
+        <vue-glide-slide v-for="planet in planets.length" :key="planet">
+          <PlanetCard v-bind:planet="planets[planet-1]" />
+        </vue-glide-slide>
+      </vue-glide>
+    </div>
+    <div class="planet-cards image--sm" v-if="isPlanetFetch">
+      <vue-glide :perView="1" :bullet="true" :bound="true" :rewind="false">
         <vue-glide-slide v-for="planet in planets.length" :key="planet">
           <PlanetCard v-bind:planet="planets[planet-1]" />
         </vue-glide-slide>
@@ -26,5 +33,6 @@ export default {
   flex-wrap: wrap;
   margin-bottom: 10px;
   justify-content: center;
+  align-items: center;
 }
 </style>
