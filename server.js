3479d44969810cf86e4a6f96f836cdb309018129
Benjamin Mark <mquadrant@gmail.com>
2019-09-30 13:31:29 +0100

    redirected server to client when page is reload

--- server.js
+++ server.js
@@ -6,3 +6,11 @@ app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log("server started " + port);
+
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "/dist/index.html"), function(err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
